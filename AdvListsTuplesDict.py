05e1afe4579de2c9bc5250a30df3cb5a6f8dd2c2
Rohit Kumar <rohitk.in@gmail.com>
2020-04-26 22:27:59 +0530

    adding new files for advanced lists and args,kwargs

---
+++ AdvListsTuplesDict.py
@@ -0,0 +1,8 @@
+'''
+In this section we will see some advanced features of Lists e.g. List comprehension.
+We will also see tuples which are just unmutable lists i.e. immutable ordered sequence 
+of python objects.Once declared they cannot be changed.
+We will also see Dictionaries that is an unordered collection of key value pair.
+Lastly we will quickly see sets and booleans
+
+'''
