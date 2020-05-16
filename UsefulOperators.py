4e04354d1b097252b41b77a02559d2b9a4d91a94
Rohit Kumar <rohitk.in@gmail.com>
2020-05-17 01:33:39 +0530

    operators provided by python

---
+++ UsefulOperators.py
@@ -0,0 +1,11 @@
+#######################################################################
+# this file has some useful operators that python provides
+# the operators we will discuss are 
+# 1. range
+# 2. enumerate
+# 3. zip
+# 4. in operator
+# 5. min and max
+# 6. random
+# 7. input
+#######################################################################
